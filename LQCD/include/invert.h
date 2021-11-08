f273affe8ae77f690ed6cbca06e82958c4954e04
Ta1ly <40461087+Ta1ly@users.noreply.github.com>
2021-11-08 23:13:54 +0800

    add rank invert

--- LQCD/include/invert.h
+++ LQCD/include/invert.h
@@ -20,5 +20,13 @@ int CGinvert(lattice_fermion &src, lattice_fermion &dest, lattice_gauge &U, cons
 
 void Dslash(lattice_fermion &src, lattice_fermion &dest, lattice_gauge &U, const double mass,
             const bool dagger);
-
+void Dslash(lattice_fermion &src, lattice_fermion &dest, lattice_gauge &U, const double mass,
+            const bool dagger,
+            int * N_sub,int rank, int size, 
+            const int nodenum_x_b, const int nodenum_x_f, 
+            const int nodenum_y_b, const int nodenum_y_f, 
+            const int nodenum_z_b, const int nodenum_z_f, 
+            const int nodenum_t_b, const int nodenum_t_f, 
+            int subgrid_vol, const int x_p
+                );
 #endif //LATTICE_invert_H
